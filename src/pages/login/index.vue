c4f3bf0a16df58830a8c5121fbb6b79b0297d8ef
bvolcan <brunovolcan@gmail.com>
2024-10-03 11:29:00 -0300

    feat: initial login page

--- src/pages/login/index.vue
+++ src/pages/login/index.vue
@@ -17,12 +17,31 @@ function handleSubmit() {
 
 <template>
     <div flex="~" items="center" justify="center" bg="#f1faee">
-        <div flex="~ col" w="200" h="130" shadow="2xl" rounded="2xl" justify="center" items="center" bg="#A8DADC" >
-            <h1>Hello login</h1>
-            <el-form :model="formLogin">
-                <el-input v-model="formLogin.email"/>
-                <el-input v-model="formLogin.password" type="password"/>
-                <el-button @click="handleSubmit">
+        <div
+            flex="~ col"
+            w="150"
+            h="100"
+            p="6"
+            shadow="2xl"
+            rounded="2xl"
+            justify="center"
+            items="center"
+            bg="white"
+        >
+            <el-text size="large" text="!3xl" m="!2">Login</el-text>
+            <el-form :model="formLogin" flex="~ col" w="full" items="center">
+                <div flex="~ col" m="t-4 b-8" w="75%">
+                    <el-form-item label-position="top" label="E-mail">
+                        <el-input v-model="formLogin.email" required size="large" placeholder="nome@email.com"/>
+                    </el-form-item>
+                    <el-form-item label-position="top" label="Senha" m="!b-2">
+                        <el-input v-model="formLogin.password" required type="password" size="large" show-password placeholder="**********"/>
+                    </el-form-item>
+                    <div>
+                        <el-link class="!text-[#457B9D]" :underline="false">Esqueceu sua senha?</el-link>
+                    </div>
+                </div>
+                <el-button w="75%" size="large" color="#457B9D" @click="handleSubmit">
                     Login
                 </el-button>
             </el-form>
